b173c7086b33dfd7cc65b4d09dce10bc6a3160a2
palashjain2801 <palashjain2801@gmail.com>
2019-10-23 21:50:55 -0500

    list_bucket

--- ITMO-544/Mid_Term/create-env.sh
+++ ITMO-544/Mid_Term/create-env.sh
@@ -4,7 +4,10 @@ sudo apt-get -y install apache2
 sudo systemctl enable apache2
 sudo systemctl start apache2
 
-sudo apt-get install php 
+sudo apt-get -y install php 
 # make sure libapache2-mod-php7.2 is there
-sudo apt-get install php-gd mysql-server php-mysql php-mysql mysql-server
+sudo apt-get -y install php-gd mysql-server php-mysql php-mysql mysql-server
 #library for image processing 
+
+
+sudo apt-get install simplexml php7.2.xml

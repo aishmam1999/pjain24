e82ad7ccc8ee81427851d7f5f6f8d8fa5c995006
palashjain2801 <palashjain2801@gmail.com>
2019-10-23 19:57:01 -0500

    midterm-started

--- ITMO-544/Mid_Term/create-env.sh
+++ ITMO-544/Mid_Term/create-env.sh
@@ -3,3 +3,8 @@ sudo apt-get -y install apache2
 
 sudo systemctl enable apache2
 sudo systemctl start apache2
+
+sudo apt-get install php 
+# make sure libapache2-mod-php7.2 is there
+sudo apt-get install php-gd mysql-server
+#library for image processing 

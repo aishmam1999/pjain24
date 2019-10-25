80804ac97ca43ae98c62bdde06bf9fa9f936e357
palashjain2801 <palashjain2801@gmail.com>
2019-10-25 13:24:54 -0500

    Week-7 added

--- ITMO-544/Mid_Term/create-env.sh
+++ ITMO-544/Mid_Term/create-env.sh
@@ -6,7 +6,7 @@ sudo systemctl start apache2
 
 sudo apt-get -y install php 
 # make sure libapache2-mod-php7.2 is there
-sudo apt-get -y install php-gd mysql-server php-mysql php-mysql mysql-server
+sudo apt-get -y install php php-gd mysql-server php-mysql php-mysql mysql-server simplexml php7.2.xml
 #library for image processing 
 
 

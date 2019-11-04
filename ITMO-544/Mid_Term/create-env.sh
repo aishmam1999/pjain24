9896280fbd80e044d6cab988918149f8135d2cdf
palashjain2801 <palashjain2801@gmail.com>
2019-11-03 20:04:40 -0600

    update echo

--- ITMO-544/Mid_Term/create-env.sh
+++ ITMO-544/Mid_Term/create-env.sh
@@ -62,59 +62,46 @@ fi
 sudo apt-get install ntpdate
 sudo ntpdate 0.amazon.pool.ntp.org                                                                                                        
 echo "***********************************Running the EC2 instances**********************************************"
-echo "----------------------------------------------------------"
 #run ec2 instances
 aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $4 --user-data file://install-app-env.sh --security-group-ids $5 --iam-instance-profile Name=$6 --subnet-id $7
 #wait till instances are in running state
 echo "*********************************waiting for instance status okay*******************************************************"
-echo "----------------------------------------------------------"
 
 aws ec2 wait instance-status-ok
 
-echo ""
 echo "***************************************EC2 Instances Deployed**************************************************************"
-echo "----------------------------------------------------------"
 
 #get the instance ids of the running instances
 MYID=`aws ec2 describe-instances --query 'Reservations[].Instances[].[State.Name, InstanceId]' --output text | grep running | awk '{print $2}'`                                                                                                                                
 echo "********************************************Creating load balancer*************************************************************"
-echo "----------------------------------------------------------"
 
 #create load balancer
 aws elb create-load-balancer --load-balancer-name pjain24-load-balancer --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --subnets $7 
 #aws elb create-load-balancer --load-balancer-name pjain24-load-balancer --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --subnets subnet-8f3249b1
 
 echo "**********************************************Performing health check***************************************************************"
-echo "----------------------------------------------------------"
 #health check
 aws elb configure-health-check --load-balancer-name pjain24-load-balancer --health-check Target=HTTP:80/,Interval=30,UnhealthyThreshold=2,HealthyThreshold=2,Timeout=3
 
 echo " ****************************************creating cookie policy**************************************************************************"
-echo "----------------------------------------------------------"
 #create cookie policy
 aws elb create-lb-cookie-stickiness-policy --load-balancer-name pjain24-load-balancer --policy-name my-duration-cookie-policy
 #register ec2 instances with load balancer
 
 echo "****************************************** Register instance to load balancer ************************************************************"
-echo "----------------------------------------------------------"
 aws elb register-instances-with-load-balancer --load-balancer-name pjain24-load-balancer --instances $MYID
 
 echo "******************************************* Waiting to Register instance to load balancer ************************************************"
-echo "----------------------------------------------------------"
 #wait for instances to be registered
 aws elb wait any-instance-in-service --load-balancer-name pjain24-load-balancer --instances $MYID
 
 echo "********************************************** Finished registering target instances ***************************************************"
-echo "----------------------------------------------------------"
 echo "*********************************************** Creating DB - instance ****************************************************************"
-echo "----------------------------------------------------------"
 #create db instance
 aws rds create-db-instance --allocated-storage 20 --db-instance-class db.t2.micro --db-instance-identifier pjain24-instance --engine mysql --master-username master --master-user-password p4ssw0rd
 
 echo "**************************************************** Created RDS instance **********************************************************************"
-echo "----------------------------------------------------------"
 echo "*************************************************** wait for DB - instance to be available ****************************************************"
-echo "----------------------------------------------------------"
 # wait for DB instace avaibility 
 aws rds wait db-instance-available --db-instance-identifier pjain24-instance
 #connect to sql

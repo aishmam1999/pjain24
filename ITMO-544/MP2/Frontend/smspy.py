a113284bc3e5d1002c25dda3ffdf93ef771e2345
palashjain2801 <palashjain2801@gmail.com>
2019-11-30 22:00:25 -0600

    update

--- ITMO-544/MP2/Frontend/smspy.py
+++ ITMO-544/MP2/Frontend/smspy.py
@@ -1,4 +1,17 @@
 import boto3
 
-client = boto3.client('SNS')
-client.publish(PhoneNumber = "+1 312 678 6501", Message="Testing masg from py code")
+# client = boto3.client('SNS')
+# client.publish(PhoneNumber = "+1 312 678 6501", Message="Testing masg from py code")
+import boto3
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+table = dynamodb.Table('RecordsPal')
+        #response = table.put_item(Item={ 'S3finishedurl': {'S':url}})
+table.update_item(
+        Key={
+        'Receipt': Receipt,
+        },
+        UpdateExpression='SET S3finishedurl = :val1',
+        ExpressionAttributeValues={
+          ':val1': "chala chala",
+        }

a6e554856f0e6e967a952ab973924267aef8db6d
palashjain2801 <palashjain2801@gmail.com>
2019-11-30 21:54:35 -0600

    smpy

--- ITMO-544/MP2/Frontend/smspy.py
+++ ITMO-544/MP2/Frontend/smspy.py
@@ -1,7 +1,4 @@
 import boto3
 
 client = boto3.client("SNS")
-client.publish(
-    PhoneNumber = "+1 312 678 6501",
-    Message="Testing masg from py code"
-)
+client.publish(PhoneNumber = "+1 312 678 6501", Message="Testing masg from py code")
